7eb0df728f8644841982114634cfa6a4c78b1686
lbstc888@163.com <lbstc888@163.com>
2020-06-30 11:53:06 +0800

    表单

--- part2/src/index.js
+++ part2/src/index.js
@@ -25,15 +25,45 @@ const notes = [
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const newNoteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+
+    setNotes(notes.concat(newNoteObject));
+    setNewNote('')
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event)=> {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={()=>setShowAll(!showAll)}>{showAll?'All':'important'}</button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note}/>
           )}
       </ul>
+      <form onSubmit={addNote}> 
+          <input value={newNote} onChange={handleNoteChange} />
+          <button type="submit">save</button>
+      </form>
     </div>
   )
 }
